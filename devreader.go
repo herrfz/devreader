2853fba18d921d6bcf932f59d44005d036460872
Eriza Fazli <erizzaaaaa@gmail.com>
2014-12-20 21:45:41 +0100

    added devreader.go

---
+++ devreader.go
@@ -0,0 +1,31 @@
+// this package abstracts I/O-device reader into channel
+package devreader
+
+type DeviceReader interface {
+	ReadDevice() ([]byte, error)
+}
+
+func MakeChannel(dev DeviceReader) <-chan []byte {
+	c := make(chan []byte)
+	go func() {
+		for {
+			buf, err := dev.ReadDevice()
+			if err != nil {
+				if err.Error() == "DONTPANIC" {
+					// non-critical error from caller
+					continue
+				} else if err.Error() == "PANIC" {
+					// critical error
+					break
+				} else {
+					// on all other errors, stop goroutine
+					break
+				}
+
+			} else {
+				c <- buf
+			}
+		}
+	}()
+	return c
+}
